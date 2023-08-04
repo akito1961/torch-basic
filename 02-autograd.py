120315f53972f02220f49022e073a663f7776541
akito1961 <98795718+akito1961@users.noreply.github.com>
2023-08-04 17:00:13 +0700

    02-autograd.py

---
+++ 02-autograd.py
@@ -0,0 +1,38 @@
+import torch
+
+x = torch.randn(3, requires_grad=True)
+
+print(x)
+
+y = x + 2
+print(y)
+
+z = y*y*2
+# z = z.mean()
+print(z)
+
+v = torch.tensor([0.1, 1.0, 0.001], dtype=torch.float32)
+z.backward(v) #dz/dx
+print(x.grad)
+
+##### Prevent Gradiant History
+# x.requires_grad_(False)
+# print(x)
+# x.detach()
+# with torch.no_grad():
+
+
+weights = torch.ones(4, requires_grad=True)
+
+for epoch in range(3):
+    model_output = (weights * 3).sum()
+    
+    model_output.backward()
+    
+    print(weights.grad)
+    
+    weights.grad.zero_() #clear gradient
+    
+# optimizer = torch.optim.SGD(weights, lr=0.01)
+# optimizer.step()
+# optimizer.zer_grad()
